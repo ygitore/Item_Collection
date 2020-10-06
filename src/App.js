1a972555825c5035178e74a044c9ada5f97c2747
yitbarekgitore <yitbareky@gmail.com>
2020-10-05 19:13:09 -0500

    fetching data from unsplash api

--- src/App.js
+++ src/App.js
@@ -1,21 +1,31 @@
+import Axios from 'axios';
 import React, { Component } from 'react';
 import './App.css';
 import SearchBar from './SearchBar';
-import SearchResult from './SearchBar';
+require('dotenv').config()
 
 class App extends Component {
-  onFormSubmit = (term) => {
-    console.log(term)
+  state = {images:[]}
+  onFormSubmit = async term => {
+    const response = await Axios.get('https://api.unsplash.com/search/photos', {
+      params:{query:term},
+      headers:{
+        Authorization: `Client-ID ${REACT_APP_UNSPLASH_kEY}`
+      }
+    })
+    this.setState({images:response.data.results})
   }
   render()
-  {
+  {   
+    console.log(this.state.images) 
+    const images = this.state.images.map(image=><img key={image.id} alt={image.alt_description} src={image.urls.regular} />)
     return (
       <div className="App">
         <SearchBar onSubmit = {this.onFormSubmit} />
+        {images}
       </div>
     );
   }
 }
 
 export default App;
-  //GJmLCx7tMPxEPVrsU1W9DuFIi3p90fYRGHCLsIXbisQ
